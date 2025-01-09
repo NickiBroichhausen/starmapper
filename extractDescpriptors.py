d09fd33dcc81cc4e3c686c51001dce6c7340703a
summercamp24.ivi.awz@gmail.com <nbro.coc.1@gmail.com>
2025-01-09 15:47:11 +0100

    allow pickle

--- extractDescpriptors.py
+++ extractDescpriptors.py
@@ -6,6 +6,6 @@ import os
 def analyse(image_path, target_folder, visualisation=False):
     image = ss.load_images([image_path])[0]
     _, descriptors = ss.get_descriptors(image, visualisation)
-    np.save(os.path.join(target_folder, f"{os.path.basename(image_path)}-descriptors.npy"), descriptors)
+    np.save(os.path.join(target_folder, f"{os.path.basename(image_path)}-descriptors.npy"), descriptors, allow_pickle=True)
     print(f"descriptors: {len(descriptors)}")
     # return descriptors
