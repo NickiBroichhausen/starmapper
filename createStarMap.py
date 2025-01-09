fa2f8cdb2fb52577a11fe6d6f9048c12786863e1
summercamp24.ivi.awz@gmail.com <nbro.coc.1@gmail.com>
2025-01-09 16:27:49 +0100

    np stuff

--- createStarMap.py
+++ createStarMap.py
@@ -61,7 +61,7 @@ def createStarMap(folder, visualisation=False):
             # TODO formations at edges might be lost
             if (transformed_point[0] - 10 < prev_x_min or transformed_point[0] + 10 > prev_x_max or 
                 transformed_point[1] - 10 < prev_y_min or transformed_point[1] + 10 > prev_y_max):
-                warped_descriptors.append(warped_descriptor)
+                np.append(warped_descriptors, warped_descriptor, axis=0)
                 if transformed_point[0] < x_min:
                     x_min = transformed_point[0]
                 if transformed_point[0] > x_max:
