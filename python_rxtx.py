11c474d730a134179f169437b008248cdb612089
Nicki <nicki_broichhausen@outlook.com>
2025-01-30 15:36:48 +0000

    fix uart example after integration

--- python_rxtx.py
+++ python_rxtx.py
@@ -14,11 +14,12 @@ mission_star_mapper = False
 # Receivers
 def starSensorCommandReceiver(data):
   try:
-    unpacked = struct.unpack("B", data)
+    unpacked = struct.unpack("B3x", data)
     print("stm sends data: {}".format(unpacked[0]))
     if(unpacked[0]):
       ready_for_picture = True
   except Exception as e:
+    print("commandRecievcer")
     print(e)
     print(data)
     print(len(data))
@@ -40,6 +41,7 @@ def satellite_mode_receiver(data):
     else:
       mission_star_mapper = False
   except Exception as e:
+
     print(e)
     print(data)
     print(len(data))
@@ -49,6 +51,7 @@ def pos_receiver(data):
     unpacked = struct.unpack("ffff", data)
     print("stm sends pos data: {} {} {}".format(unpacked[0],unpacked[1],unpacked[2]))
   except Exception as e:
+
     print(e)
     print(data)
     print(len(data))
@@ -78,17 +81,20 @@ sensor_index = 0
 while True:
   # Dummy sensor data
 
-  #if(mission_star_mapper):
+  if(mission_star_mapper):
     # Pack sensor data to a struct that RODOS recognizes
-  #  if(ready_for_picture):
+    if(ready_for_picture):
       #take picture then:
-  sensor_struct = struct.pack("B",1)
-  ras2stmCommands.publish(sensor_struct)#command "picture made"
-  ready_for_picture = False
+      sensor_struct = struct.pack("B",1)
+      ras2stmCommands.publish(sensor_struct)#command "picture made"
+      ready_for_picture = False
 
-  #if(control_mode_ai_vel | control_mode_ai_pos):
-  data_struct = struct.pack("f",0.123456789) #test value
-  ras2stmControlValue.publish(data_struct)
 
-  print("sent") 
+
+      print("sent command") 
+  if(control_mode_ai_vel | control_mode_ai_pos):
+    data_struct = struct.pack("f",0.123456789) #test value
+    ras2stmControlValue.publish(data_struct)
+
+    print("sent") 
   time.sleep(1)
