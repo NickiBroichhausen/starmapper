270d26783b058e6447188f42916811aa4250958a
summercamp24.ivi.awz@gmail.com <nbro.coc.1@gmail.com>
2025-02-03 11:40:41 +0100

    add functionality to main_rxtx

--- main_rxtx.py
+++ main_rxtx.py
@@ -7,51 +7,75 @@ import struct
 import rodosmwinterface as rodos
 rodos.printTopicInit(enable=True)
 
-ready_for_picture = False
+RODOS_ready_for_picture = False
 control_mode_ai_vel = False
 control_mode_ai_pos = False
 RODOS_take_pictures = False
 RODOS_pictures_done = False
 RODOS_get_attitude = False
+RODOS_set_folder = False
+RODOS_pos = 0
 
 # Receivers
 def starSensorCommandReceiver(data):
+  global RODOS_ready_for_picture
+  global RODOS_take_pictures
   try:
     unpacked = struct.unpack("B", data)
     print("stm sends data: {}".format(unpacked[0]))
-    if(unpacked[0]):
-      ready_for_picture = True
+    if(unpacked[0]==1): #take next picture
+      RODOS_ready_for_picture = True
+    if(unpacked[0]==0): # abort
+      RODOS_take_pictures = False
+  except Exception as e:
+    print(e)
+    print(data)
+    print(len(data))
+
+def setFolder(data):
+  global RODOS_set_folder
+  try:
+    RODOS_set_folder = struct.unpack("B", data)[0]
+    print("stm sends set folder: {}".format(RODOS_set_folder))
   except Exception as e:
     print(e)
     print(data)
     print(len(data))
 
 def satellite_mode_receiver(data):
+  global RODOS_take_pictures
+  global RODOS_pictures_done
+  global RODOS_get_attitude
   try:
     unpacked = struct.unpack("BBB", data)
     print("stm sends data: {} {} {}".format(unpacked[0],unpacked[1],unpacked[2]))
-    if(unpacked[1]==3):
-      control_mode_ai_vel = True
+    # if(unpacked[1]==3):
+    #   control_mode_ai_vel = True
+    # else:
+    #   control_mode_ai_vel = False
+    # if(unpacked[1]==4):
+    #   control_mode_ai_pos = True
+    # else:
+    #   control_mode_ai_pos = False
+    if(unpacked[0]==2): # attitude is used
+      RODOS_get_attitude = True
     else:
-      control_mode_ai_vel = False
-    if(unpacked[1]==4):
-      control_mode_ai_pos = True
-    else:
-      control_mode_ai_pos = False
-    if(unpacked[2]==2):
+      RODOS_get_attitude = False
+    if(unpacked[2]==2):  #start to create star map
       RODOS_take_pictures = True
     else:
-      RODOS_take_pictures = False
-    #how can i change here to next step get atitude
+      RODOS_pictures_done = True
   except Exception as e:
     print(e)
     print(data)
     print(len(data))
 
 def pos_receiver(data):
+  global RODOS_pos
   try:
-    unpacked = struct.unpack("fffB", data)
+    unpacked = struct.unpack("ffff", data)
     print("stm sends pos data: {} {} {}".format(unpacked[0],unpacked[1],unpacked[2]))
+    RODOS_pos = unpacked[2]
   except Exception as e:
     print(e)
     print(data)
@@ -61,6 +85,7 @@ def pos_receiver(data):
 ras2stmCommands = rodos.Topic(1021)
 stm2rasCommands = rodos.Topic(1020)
 ras2stmControlValue = rodos.Topic(1024)
+stm2rasSetFolder = rodos.Topic(1022)
 stm2rasMode = rodos.Topic(1014) #1011
 stm2rasPos = rodos.Topic(1011)
 
@@ -73,12 +98,10 @@ gwUart.run()
 stm2rasCommands.addSubscriber(starSensorCommandReceiver)
 stm2rasMode.addSubscriber(satellite_mode_receiver)
 stm2rasPos.addSubscriber(pos_receiver)
+stm2rasSetFolder.addSubscriber(setFolder)
 #sender
 gwUart.forwardTopic(ras2stmCommands)
 gwUart.forwardTopic(ras2stmControlValue)
-
-# sensor_index = 0
-
 
 
 ################### Starmap stuff###################
@@ -90,62 +113,78 @@ from time import sleep
 import os
 import threading
 import traceback
-import StarSift as ss
-import serial
+# import StarSift as ss
 
-folder = "star_data"
+folder = "1"
+visualisation = False
 
-visualisation = True
+while True:  
+  try:
+    if(RODOS_take_pictures):
+      # check if folder exist
+      if not os.path.exists(folder):
+        os.makedirs(folder)
+      
+      camera.init()
+      while True:
+        if((RODOS_ready_for_picture)):
+          #Take picture
+          image_path = os.path.join(folder, f"image_{int(RODOS_pos)}.jpg")
+          camera.take_picture(image_path)
+          threading.Thread(target=extractDescpriptors.analyse, args=(image_path, folder, visualisation)).start()
+          
+          #command "picture made"
+          sensor_struct = struct.pack("B",1)
+          stm2rasCommands.publish(sensor_struct)
+          RODOS_ready_for_picture = False
 
-while True:
-  # Dummy sensor data
+        if(RODOS_pictures_done):
+          #stitch map
+          createStarMap.createStarMap(folder, visualisation)
+          #star mapping done
+          sensor_struct = struct.pack("B",0)
+          stm2rasCommands.publish(sensor_struct)
 
-  if(RODOS_take_pictures):
-    # check if folder exist
-    if not os.path.exists(folder):
-      os.makedirs(folder)
-    
-    camera.init()
-    angle = 0
-    while True:  # TODO when break??
-      if(ready_for_picture):
-        #Take picture
-        image_path = os.path.join(folder, f"image_{angle}.jpg")
+          RODOS_pictures_done = False
+          break
+
+      camera.deinit()
+      RODOS_take_pictures = False
+
+    if(RODOS_get_attitude):
+      while True:
+        image_path = "current.jpg"
+        camera.init()
         camera.take_picture(image_path)
-        threading.Thread(target=extractDescpriptors.analyse, args=(image_path, folder, visualisation)).start()
-        angle = angle + 1
-        
-        #command "picture made"
-        sensor_struct = struct.pack("B",1)
+        camera.deinit()
+        attidude = AttitudeDetermination.find_attitude(folder, 
+                                                    image_path,
+                                                    visualisation)
+        #send attitude
+        sensor_struct = struct.pack("f",attidude)  # TODO what to return here? float?
         stm2rasCommands.publish(sensor_struct)
-        ready_for_picture = False
 
-      if(RODOS_pictures_done):
-        #stitch map
-        createStarMap.createStarMap(folder, visualisation)
-        RODOS_pictures_done = False
-        break
+        if(not RODOS_get_attitude):
+          break
+      RODOS_get_attitude = False
 
+    if(RODOS_set_folder):
+      #set folder
+      folder = RODOS_set_folder
+      # check if folder exist
+      if not os.path.exists(folder):
+          os.makedirs(folder)
+      RODOS_set_folder = False
+
+    # if(control_mode_ai_vel | control_mode_ai_pos):
+    #   data_struct = struct.pack("f",0.123456789) #test value for torque
+    #   ras2stmControlValue.publish(data_struct)
+
+    time.sleep(1)
+
+  except Exception as e:
+    print(e)
+    print(traceback.format_exc())
     camera.deinit()
-    RODOS_take_pictures = False
-
-  if(RODOS_get_attitude):
-    image_path = "current.jpg"
-    camera.init()
-    camera.take_picture(image_path)
-    camera.deinit()
-    attidude = AttitudeDetermination.find_attitude(folder, 
-                                                image_path,
-                                                visualisation)
-    #send attitude
-    sensor_struct = struct.pack("f",attidude)  # TODO what to return here? float?
-    stm2rasCommands.publish(sensor_struct)
-    RODOS_get_attitude = False
 
 
-  if(control_mode_ai_vel | control_mode_ai_pos):
-    data_struct = struct.pack("f",0.123456789) #test value
-    ras2stmControlValue.publish(data_struct)
-
-
-  time.sleep(1)
